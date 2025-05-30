c243a42af4dc0c83fa7dd806e97f0c0e9965908b
PavlomirDoitchev <pavlomirdoitchev@gmail.com>
2025-05-30 17:29:21 +0300

    MInorAdjustments to basic trap code

--- Environment/Traps/SpikeTrap.cs
+++ Environment/Traps/SpikeTrap.cs
@@ -1,25 +1,25 @@
 using Assets.Scripts.Combat_Logic;
-using Assets.Scripts.Player;
 using UnityEngine;
 
 public class SpikeTrap : MonoBehaviour
 {
+    [SerializeField] GameObject movingSpikesReference;
     [SerializeField] private int damage;
+    [Header("Timer should equal the cooldown!")]
     [SerializeField] float timer = 1;
     [SerializeField] float spikeCooldown = 1f;
-    [SerializeField] GameObject spikes;
-    [SerializeField] float spikeHeight = 0.5f;
+    [SerializeField] float spikeRiseLimit = 0.5f;
     private float spikeOrignalHeight = -0.266f;
     bool hasTakenDamage = false;
     private void OnTriggerStay(Collider other)
     {
         if (other.TryGetComponent<IDamagable>(out var damagable))
         {
-            Vector3 spikePosition = spikes.transform.localPosition;
+            Vector3 spikePosition = movingSpikesReference.transform.localPosition;
             timer -= Time.deltaTime;
             if (timer <= 0 && !hasTakenDamage)
             {
-                spikes.transform.localPosition = new Vector3(spikePosition.x, spikeHeight, spikePosition.z);
+                movingSpikesReference.transform.localPosition = new Vector3(spikePosition.x, spikeRiseLimit, spikePosition.z);
                 damagable.TakeDamage(damage);
                 hasTakenDamage = true;
             }
@@ -29,9 +29,9 @@ public class SpikeTrap : MonoBehaviour
     {
         if (other.TryGetComponent<IDamagable>(out var damagable))
         {
-            Vector3 spikePosition = spikes.transform.localPosition;
+            Vector3 spikePosition = movingSpikesReference.transform.localPosition;
             spikePosition.y = spikeOrignalHeight;
-            spikes.transform.localPosition = spikePosition;
+            movingSpikesReference.transform.localPosition = spikePosition;
             if (timer <= spikeCooldown)
                 timer = spikeCooldown;
             hasTakenDamage = false;
