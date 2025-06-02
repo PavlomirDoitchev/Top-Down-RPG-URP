660fe22f0372dd1d45b39cc96165457ad13f4aa7
PavlomirDoitchev <pavlomirdoitchev@gmail.com>
2025-06-02 19:14:52 +0300

    Updating movement and adding gizmos to display path

--- Environment/Moving Platforms/MovingPlatform.cs
+++ Environment/Moving Platforms/MovingPlatform.cs
@@ -5,14 +5,33 @@ public class MovingPlatform : MonoBehaviour
     [SerializeField] GameObject startPoint;
     [SerializeField] GameObject endPoint;
     [SerializeField] GameObject platform;
-    [SerializeField] float speed = 1f;  
+    [SerializeField] float speed = 1f;
+
+    private float lerpTime = 0f;
+    private bool goingForward = true;
+
     void Update()
     {
-        platform.transform.localPosition = Vector3.Lerp(
-            startPoint.transform.position,
-            endPoint.transform.position,
-            Mathf.PingPong(Time.time, speed)
-        );
+        lerpTime += Time.deltaTime * speed;
+        if (lerpTime > 1f)
+        {
+            lerpTime = 0f;
+            goingForward = !goingForward;
+        }
+
+        if (goingForward)
+            platform.transform.position = Vector3.Lerp(startPoint.transform.position, endPoint.transform.position, lerpTime);
+        else
+            platform.transform.position = Vector3.Lerp(endPoint.transform.position, startPoint.transform.position, lerpTime);
     }
-   
+    private void OnDrawGizmos()
+    {
+        if (startPoint != null && endPoint != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(startPoint.transform.position, endPoint.transform.position);
+            Gizmos.DrawSphere(startPoint.transform.position, 0.1f);
+            Gizmos.DrawSphere(endPoint.transform.position, 0.1f);
+        }
+    }
 }
