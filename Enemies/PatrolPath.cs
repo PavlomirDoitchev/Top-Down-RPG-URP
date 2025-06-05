20095f1eded5d145b9961de13c595361cdfe6657
PavlomirDoitchev <pavlomirdoitchev@gmail.com>
2025-06-05 19:19:01 +0300

    PatrolPath beginning

---
+++ Enemies/PatrolPath.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+namespace Assets.Scripts.Enemies
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        [SerializeField] Transform[] waypoints;
+        [SerializeField] Color gizmoColor;
+        [SerializeField] float gizmoSize = 0.2f;
+        private void Start()
+        {
+            Debug.Assert(waypoints.Length > 0, "PatrolPath must have at least one waypoint assigned.");
+        }
+        private void OnDrawGizmos()
+        {
+            for (int i = 0; i < waypoints.Length; i++)
+            {
+                if (waypoints[i] == null) continue;
+                Gizmos.color = gizmoColor;
+                Gizmos.DrawSphere(waypoints[i].position, gizmoSize);
+            }
+        }
+    }
+}
