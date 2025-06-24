035053c94af27bd152dbfd4c0f945b5e909f9309
PavlomirDoitchev <pavlomirdoitchev@gmail.com>
2025-06-24 17:51:23 +0300

    improving custom anim events in inspector

---
+++ Utility/AnimationEventStateBehaviourEditor.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Scripts.Utility.Animation;
+
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts.Utility
+{
+    [CustomEditor(typeof(AnimationEventStateBehaviour))]
+    public class AnimationEventStateBehaviourEditor : Editor
+    {
+        AnimationClip previewClip;
+        float previewTime;
+
+
+    }
+}
---
+++ Utility/TestEvents.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+namespace Assets.Scripts.Utility
+{
+    public class TestEvents : MonoBehaviour
+    {
+        [SerializeField] ParticleSystem attackEffect;
+        Animator animator;
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+            if (animator == null)
+            {
+                Debug.LogError("Animator component not found on the GameObject.");
+            }
+
+        }
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                animator.Play("Combat_Unarmed_Attack");
+            }
+            
+        }
+        public void SpawnVFX()
+        {
+            if (!attackEffect.isPlaying)
+                attackEffect.Play();
+        }
+    }
+}
