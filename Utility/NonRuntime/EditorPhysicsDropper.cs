f744c8d3791b5e52f74ad759aaec7f229c20a832
PavlomirDoitchev <pavlomirdoitchev@gmail.com>
2025-09-03 00:10:18 +0300

    Custom gravity dropper for in editor quick placement

--- Utility/NonRuntime/EditorPhysicsDropper.cs
+++ Utility/NonRuntime/EditorPhysicsDropper.cs
@@ -3,46 +3,16 @@ using UnityEngine;
 
 public static class EditorPhysicsDropper
 {
-    private static Vector3 defaultGravity;
-
     [MenuItem("Tools/Drop With Physics %#d")] // Ctrl+Shift+D
     public static void DropWithPhysics()
     {
         foreach (var go in Selection.gameObjects)
         {
-            if (go.GetComponent<EditorPhysicsDrop>() == null)
-                go.AddComponent<EditorPhysicsDrop>();
-        }
+            var rb = go.GetComponent<Rigidbody>();
+            if (rb) Object.DestroyImmediate(rb);
 
-        // Save current gravity so we can restore later
-        defaultGravity = Physics.gravity;
-
-        // Use gentler gravity only while simulating in editor
-
-        Physics.simulationMode = SimulationMode.Script;
-        EditorApplication.update += Simulate;
-    }
-
-    [MenuItem("Tools/Freeze Dropped Objects %#f")] // Ctrl+Shift+F
-    public static void FreezeDroppedObjects()
-    {
-        foreach (var drop in Object.FindObjectsByType<EditorPhysicsDrop>(FindObjectsSortMode.None))
-        {
-            drop.Freeze();
-        }
-
-        EditorApplication.update -= Simulate;
-
-
-        Physics.simulationMode = SimulationMode.FixedUpdate;
-    }
-
-    private static void Simulate()
-    {
-        if (!Application.isPlaying)
-        {
-            Physics.Simulate(Time.fixedDeltaTime);
-            SceneView.RepaintAll();
+            if (!go.GetComponent<EditorGravityDrop>())
+                go.AddComponent<EditorGravityDrop>();
         }
     }
 }
